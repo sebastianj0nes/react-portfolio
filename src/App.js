6161f8326740eaedfbfc5162a0340ad4f9c2d8b9
Seb <jonesseb10@gmail.com>
2023-03-30 21:48:27 +0100

    fix netlify reroute error. Try3

--- src/App.js
+++ src/App.js
@@ -23,20 +23,4 @@ function App() {
   );
 }
 
-// <Router>
-//       <div>
-//         <NavTabs />
-//         {/* Wrap Route elements in a Routes component */}
-//         <Routes>
-//           {/* Define routes using the Route component to render different page components at different paths */}
-//           {/* Define a default route that will render the Home component */}
-//           <Route path="/" element={<Home />} />
-//           <Route path="about" element={<About />} />
-//           <Route path="blog" element={<Blog />} />
-//           {/* Define a route that will have descendant routes */}
-//           <Route path="contact/*" element={<Contact />} />
-//         </Routes>
-//       </div>
-//     </Router>
-
 export default App;
