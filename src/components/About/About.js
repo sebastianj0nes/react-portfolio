a5e78c3235ea28cde3cda1f556f848b98a0f703d
Seb <jonesseb10@gmail.com>
2023-03-31 16:31:12 +0100

    update about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,7 +14,9 @@ function About() {
                     <h1 className="d-flex justify-content-center align-items-center p-3" id="header">ABOUT</h1>
                     <p className="d-flex justify-content-center align-items-center p-3">Software Engineer curious to learn more</p>
                     <p className="text-muted d-flex justify-content-center align-items-center">Based in Bristol</p>
-                    <p className="d-flex justify-content-center align-items-center">Enjoy creative processes</p>
+                    <p className="d-flex justify-content-center align-items-center">Enjoy creative processes, exploring new topics & pushing boundaries</p>
+                    <h5 className="d-flex justify-content-center align-items-center p-3">Key interests in :</h5>
+                    <p className="d-flex justify-content-center align-items-center p-3">Blockchain Technology, AI & AGI and Machine Learning  </p>
                 </Row>
                 <Row>
                     <h1 className="d-flex justify-content-center align-items-center p-3">Contact Me</h1>
